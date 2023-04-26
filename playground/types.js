be0238fc697f7ad7dfc19b13bdfe0b5cfc6dbe17
Joel Rdz <joelrdzh@gmail.com>
2023-04-25 19:14:47 -0600

    js data types - number
    - typeof to get type
    - + - * / % **
    - Math method
    - Infinity
    - NaN

--- playground/types.js
+++ playground/types.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+/*
 const name = 'Albus';
 const middle = "Severus";
 const last = `Potter`;
@@ -29,3 +30,47 @@ const html = `
 `;
 
 document.body.innerHTML = html;
+*/
+
+const age = 30.5;
+const name = 'jon';
+
+console.log(typeof age); // number
+console.log(typeof name); // string
+console.log(typeof 123); // number
+console.log(typeof '456'); // string
+
+const a = 10;
+const b = 20;
+
+console.log(a + b); // 30
+console.log(a - b); // -10
+console.log(a * b); // 200
+console.log(a / b); // 0.5
+
+console.log(1 + '1') // '11' (string)
+console.log('10' * '10') // 100 (number)
+
+console.log(Math.round(20.5)); // 21
+console.log(Math.round(20.2)); // 20
+console.log(Math.floor(20.9)); // 20
+console.log(Math.ceil(20.1)); // 21
+console.log(Math.random()); // random value between 0-1
+
+const skittles = 20;
+const kids = 3;
+const eachKidGets = Math.floor(skittles / kids);
+const dadGets = skittles % kids;
+console.log(`Each kid gets ${eachKidGets} skittles.`);
+console.log(`Dad gets only ${dadGets} skittles.`);
+
+// const price = 10.34;
+const price = 1034;
+
+console.log(1000 ** 2000); // Infinity
+console.log(-(1000 ** 2000)); // -Infinity
+console.log(typeof Infinity); // number
+console.log(typeof -Infinity); // number
+
+console.log(typeof 10 - 'dog'); // NaN
+console.log(typeof NaN); // Number
