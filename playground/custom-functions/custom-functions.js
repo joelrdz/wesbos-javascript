c7a41e8b13e6127b22dc34a2d034ea6b114759eb
Joel Rdz <joelrdzh@gmail.com>
2023-10-03 00:06:02 -0600

    js functions returning values

--- playground/custom-functions/custom-functions.js
+++ playground/custom-functions/custom-functions.js
@@ -3,8 +3,10 @@ function calculateBill() {
   // this is the function body
   console.log('Runnning Calculate Bill');
   const total = 100 * 1.13;
-  console.log(total);
+  return total;
 }
 
 // Function Call or **Run**
-calculateBill();
+// console.log(`Your Total is ${calculateBill()}`);
+const myTotal = calculateBill();
+console.log(`Your Total is ${myTotal}`);
