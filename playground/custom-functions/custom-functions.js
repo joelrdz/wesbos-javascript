f05e0aba153926afb708a460d7632c2cb1fdbc53
Joel Rdz <joelrdzh@gmail.com>
2023-10-02 23:42:45 -0600

    js custom functions - function definition and call

---
+++ playground/custom-functions/custom-functions.js
@@ -0,0 +1,10 @@
+// Function Definition
+function calculateBill() {
+  // this is the function body
+  console.log('Runnning Calculate Bill');
+  const total = 100 * 1.13;
+  console.log(total);
+}
+
+// Function Call or **Run**
+calculateBill();
